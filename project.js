7c1cdc46147bd7fa43411b92664157106a45c1de
RavenC0re20 <79119651+RavenC0re20@users.noreply.github.com>
2021-02-16 09:01:00 +0800

    Update project.js

--- project.js
+++ project.js
@@ -1,5 +1,6 @@
 let point;
-let no = [];
+let nusno = [];
+let ntuno = [];
 function calculate(){
 let first_H2_grade = Number(document.getElementById("H21").value);
 let second_H2_grade = Number(document.getElementById("H22").value);
@@ -84,6 +85,52 @@ let nus = [nus1,nus2,nus3,nus4,nus5,nus6,nus7,nus8,nus9,nus10,nus11,nus12,nus13,
 nus20,nus21,nus22,nus23,nus24,nus25,nus26,nus27,nus28,nus29,
 nus30,nus31,nus32,nus33,nus34,nus35];
 
+let ntu = [];
+ntu[0] = {min:85,max:85,name:"Medicine*",link:"https://law.nus.edu.sg/"};
+ntu[1] = {min:85,max:85,name:"Renaissance Engineering*",link:"https://law.nus.edu.sg/"};
+ntu[2] = {min:85,max:85,name:"Data Science & Artificial Intelligence",link:"https://law.nus.edu.sg/"};
+ntu[3] = {min:85,max:85,name:"Environmental Earth Systems Science*",link:"https://law.nus.edu.sg/"};
+ntu[4] = {min:82.5,max:85,name:"Science Double Major Programmes*",link:"https://law.nus.edu.sg/"};
+ntu[5] = {min:82.5,max:85,name:"Biological Sciences*",link:"https://law.nus.edu.sg/"};
+ntu[6] = {min:82.5,max:85,name:"HASS Double Major Programme*",link:"https://law.nus.edu.sg/"};
+ntu[7] = {min:81.25,max:85,name:"Computer Science",link:"https://law.nus.edu.sg/"};
+ntu[8] = {min:80,max:85,name:"Aerospace Engineering",link:"https://law.nus.edu.sg/"};
+ntu[9] = {min:80,max:85,name:"Communication Studies*",link:"https://law.nus.edu.sg/"};
+ntu[10] = {min:78.75,max:85,name:"Computer Engineering",link:"https://law.nus.edu.sg/"};
+ntu[11] = {min:78.75,max:85,name:"Science (Education)",link:"https://law.nus.edu.sg/"};
+ntu[12] = {min:77.5,max:85,name:"Psychology",link:"https://law.nus.edu.sg/"};
+ntu[13] = {min:76.25,max:85,name:"Arts (Education)	",link:"https://law.nus.edu.sg/"};
+ntu[14] = {min:75,max:85,name:"Public Policy & Global Affairs	",link:"https://law.nus.edu.sg/"};
+ntu[15] = {min:73.75,max:85,name:"Chemical & Biomolecular Engineering	",link:"https://law.nus.edu.sg/"};
+ntu[16] = {min:73.75,max:85,name:"Chemical & Biological Chemistry	",link:"https://law.nus.edu.sg/"};
+ntu[17] = {min:72.5,max:85,name:"Accountancy*",link:"https://law.nus.edu.sg/"};
+ntu[18] = {min:72.5,max:85,name:"Sociology",link:"https://law.nus.edu.sg/"};
+ntu[19] = {min:71.25,max:85,name:"Bioengineering",link:"https://law.nus.edu.sg/"};
+ntu[20] = {min:71.25,max:85,name:"Mathematical Sciences",link:"https://law.nus.edu.sg/"};
+ntu[21] = {min:71.25,max:85,name:"Business*",link:"https://law.nus.edu.sg/"};
+ntu[22] = {min:71.25,max:85,name:"Economics",link:"https://law.nus.edu.sg/"};
+ntu[23] = {min:71.25,max:85,name:"Linguistics & Multilingual Studies*",link:"https://law.nus.edu.sg/"};
+ntu[24] = {min:70,max:85,name:"Sport Science & Management",link:"https://law.nus.edu.sg/"};
+ntu[25] = {min:68.75,max:83.75,name:"Information Engineering & Media",link:"https://law.nus.edu.sg/"};
+ntu[26] = {min:68.75,max:85,name:"Physics/ Applied Physics",link:"https://law.nus.edu.sg/"};
+ntu[27] = {min:68.75,max:85,name:"English*",link:"https://law.nus.edu.sg/"};
+ntu[28] = {min:68.75,max:81.25,name:"Philosophy*",link:"https://law.nus.edu.sg/"};
+ntu[29] = {min:67.5,max:85,name:"Environmental Engineering",link:"https://law.nus.edu.sg/"};
+ntu[30] = {min:67.5,max:85,name:"Art, Design & Media*^",link:"https://law.nus.edu.sg/"};
+ntu[31] = {min:67.5,max:83.75,name:"Chinese",link:"https://law.nus.edu.sg/"};
+ntu[32] = {min:67.5,max:85,name:"History*",link:"https://law.nus.edu.sg/"};
+ntu[33] = {min:66.25,max:85,name:"Mechanical Engineering",link:"https://law.nus.edu.sg/"};
+ntu[34] = {min:65,max:78.75,name:"Maritime Studies",link:"https://law.nus.edu.sg/"};
+ntu[35] = {min:65,max:85,name:"Materials Engineering",link:"https://law.nus.edu.sg/"};
+ntu[36] = {min:63.75,max:76.25,name:"Engineering",link:"https://law.nus.edu.sg/"};
+ntu[37] = {min:62.5,max:83.75,name:"Civil Engineering",link:"https://law.nus.edu.sg/"};
+ntu[38] = {min:62.5,max:85,name:"Electrical & Electronic Engineering",link:"https://law.nus.edu.sg/"};
+
+
+
+
+
+
 function print_courses(){
     //document.getElementById("higher").innerHTML = nus1;
     let table = document.getElementById("table1");
@@ -106,7 +153,6 @@ function print_courses(){
     let middlelink = [];
     lower = [];
     let lowerlink = [];
-    console.log("My object: ",nus[0].min);
 
     for (let i = 0; i < nus.length; i++){
         //console.log("My object: ",nus[i].max);
@@ -157,44 +203,111 @@ function print_courses(){
         }
     }
 
-    //document.getElementById("higher").innerHTML = higher;
-    //document.getElementById("higher0").innerHTML = "Better than 90th Percentile";
-
-
-    //document.getElementById("middle").innerHTML = middle;
-    //document.getElementById("middle0").innerHTML = "Between 90th and 10th percentile";
-
-
-    //document.getElementById("lower").innerHTML = lower;
-    //document.getElementById("lower0").innerHTML = "Just below 10th Percentile (One H2 Grade or less difference)";
-
-    no = [];
+    nusno = [];
     for (let i = 0; i < nus.length; i++){
         if (point < Number(nus[i]["min"]) - 2.5){
-            no.push("<a href=" +nus[i]["link"]+" target='_blank'> " + nus[i]["name"] + " (" +nus[i].min+")" + "</a>");
+            nusno.push("<a href=" +nus[i]["link"]+" target='_blank'> " + nus[i]["name"] + " (" +nus[i].min+")" + "</a>");
         }
     }
-    document.getElementById("no0").innerHTML = "Below 10th Percentile by more than one H2 grade";
-    document.getElementById("no").innerHTML = "<p onclick='show_text()' id='p1'> click to show courses</p>";
+    document.getElementById("nusno0").innerHTML = "Below 10th Percentile by more than one H2 grade";
+    hide_text1();
+
+    //table for ntu
+    higher = [];
+    higherlink = [];
+    middle = [];
+    middlelink = [];
+    lower = [];
+    lowerlink = [];
+    for (let i = 0; i < ntu.length; i++){
+        //console.log("My object: ",nus[i].max);
+        if (point > Number(ntu[i].max)){
+            higher.push(" " + ntu[i].name + " (" +ntu[i].min+")");
+            higherlink.push(ntu[i].link);
+        }
+        if (point >= Number(ntu[i]["min"]) && point <= Number(ntu[i]["max"])){
+            middle.push(" " + ntu[i]["name"] + " (" +ntu[i].min+")");
+            middlelink.push(ntu[i].link);
+        }
+        if (point >= Number(ntu[i]["min"]) - 2.5 && point < Number(ntu[i]["min"])){
+            lower.push(" " + ntu[i]["name"] + " (" +ntu[i].min+")");
+            lowerlink.push(ntu[i].link);
+        }
+    }
+    compare = higher.length;
+    if (compare < middle.length){
+        compare = middle.length
+    }
+    if (compare < lower.length){
+        compare = lower.length
+    }
+    table = document.getElementById("table2");
+    x = table.rows.length;
+    for (let i = 0; i < x - 1; i++){
+         table.deleteRow(1); //needs to be 1 as row index changes
+    }
+    for (let i = 0; i < compare; i++){
+        row = table.insertRow(i + 1);
+        cell1 = row.insertCell(0);
+        cell2 = row.insertCell(1);
+        cell3 = row.insertCell(2);
+        if (higher.length > i){
+            cell1.innerHTML = "<a href=" +higherlink[i]+" target='_blank'>" + higher[i] +"</a>";
+        }
+        else{
+            cell1.innerHTML = " ";
+        }
+        if (middle.length > i){
+            cell2.innerHTML = "<a href=" +middlelink[i]+" target='_blank'>" + middle[i] +"</a>";
+        }
+        else{
+            cell2.innerHTML = " ";
+        }
+        if (lower.length > i){
+            cell3.innerHTML = "<a href=" +lowerlink[i]+"target='_blank'>" + lower[i] +"</a>";
+        }
+        else{
+            cell3.innerHTML = " ";
+        }
+    }
+    ntuno = [];
+    for (let i = 0; i < ntu.length; i++){
+        if (point < Number(ntu[i]["min"]) - 2.5){
+            ntuno.push("<a href=" +ntu[i]["link"]+" target='_blank'> " + ntu[i]["name"] + " (" +ntu[i].min+")" + "</a>");
+        }
+    }
+    document.getElementById("ntuno0").innerHTML = "Below 10th Percentile by more than one H2 grade";
+    hide_text2();
 
 }
 
 function hide(){
-    var x = document.getElementById("table");
+    var x = document.getElementById("table1");
+    var y = document.getElementById("table2");
     x.style.display = "none";
+    y.style.display = "none";
 }
 
 function showtable(){
-    var x = document.getElementById("table");
+    var x = document.getElementById("table1");
+    var y = document.getElementById("table2");
     x.style.display = "block";
+    y.style.display = "block";
 }
 
-function show_text(){
-    document.getElementById("no").innerHTML = "<p id='p1'>" +no+ " <strong onclick='hide_text()'>(click to hide)</strong></p>";
+function show_text1(){
+    document.getElementById("nusno").innerHTML = "<p id='p1' style='color:purple;'>" +nusno+ " <strong onclick='hide_text1()'>(click to hide)</strong></p>";
 
 }
-function hide_text(){
-    document.getElementById("no").innerHTML = "<p onclick='show_text()' id='p1'> click to show courses</p>";
+function hide_text1(){
+    document.getElementById("nusno").innerHTML = "<p onclick='show_text1()' id='p1'> click to show courses</p>";
+}
+function show_text2(){
+    document.getElementById("ntuno").innerHTML = "<p id='p2' style='color:purple;'>" +ntuno+ " <strong onclick='hide_text2()'>(click to hide)</strong></p>";
+
+}
+function hide_text2(){
+    document.getElementById("ntuno").innerHTML = "<p onclick='show_text2()' id='p2'> click to show courses</p>";
 }
 
 function call(){
